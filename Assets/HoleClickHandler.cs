0db406b01923881d454d8479797ef2f30bf56b06
rhsehfm33 <rhsehfm33@gmail.com>
2024-11-16 00:25:55 +0900

    feat 못을 박을 수 있는지 없는지 체크하는 로직 구현

---
+++ Assets/HoleClickHandler.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class HoleClickHandler : MonoBehaviour
+{
+    private Collider2D myCollider;
+
+    private void Start()
+    {
+        myCollider = GetComponent<Collider2D>();
+    }
+
+    // 못을 넣을 수 있는지 체크
+    public bool CanPutNail()
+    {
+        // 감지된 Collider를 저장할 배열
+        Collider2D[] colliders = new Collider2D[10]; // 최대 10개까지 감지
+        ContactFilter2D filter = new ContactFilter2D();
+        filter.SetLayerMask(LayerMask.GetMask("Nail", "Plank1"));
+        int nailAndPlankCount = myCollider.OverlapCollider(filter, colliders);
+        return nailAndPlankCount == 0;
+    }
+
+    // 못을 뺄 수 있는지 체크
+    public bool CanPickNail()
+    {
+        // 감지된 Collider를 저장할 배열
+        Collider2D[] colliders = new Collider2D[1]; // 최대 10개까지 감지
+        ContactFilter2D filter = new ContactFilter2D();
+        filter.SetLayerMask(LayerMask.GetMask("Nail"));
+        int nailCount = myCollider.OverlapCollider(filter, colliders);
+        return nailCount == 1;
+    }
+
+    private void OnMouseDown()
+    {
+        if (CanPutNail())
+        {
+            Debug.Log("못을 박을 수 있습니다!");
+        }
+        if (CanPickNail())
+        {
+            Debug.Log("못을 뺄 수 있습니다!");
+        }
+    }
+}
